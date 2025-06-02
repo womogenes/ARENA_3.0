86e1f83d9897c8299124386f5c0ea9473350c488
William Y. Feng <womogenes2@gmail.com>
2025-06-01 21:26:39 -0700

    Do more stuff with CNNs

--- chapter0_fundamentals/exercises/part2_cnns/0.2_cnns_resnets.py
+++ chapter0_fundamentals/exercises/part2_cnns/0.2_cnns_resnets.py
@@ -141,3 +141,242 @@ class SimpleMLP(nn.Module):
 
 tests.test_mlp_module(SimpleMLP)
 tests.test_mlp_forward(SimpleMLP)
+
+
+# %%
+# Dataloader
+
+MNIST_TRANSFORM = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(0.1307, 0.3081),
+    ]
+)
+
+def get_mnist(trainset_size: int = 10_000, testset_size: int = 1_000) -> tuple[Subset, Subset]:
+    """Returns a subset of MNIST training data."""
+
+    # Get original datasets, which are downloaded to "chapter0_fundamentals/exercises/data" for future use
+    mnist_trainset = datasets.MNIST(exercises_dir / "data", train=True, download=True, transform=MNIST_TRANSFORM)
+    mnist_testset = datasets.MNIST(exercises_dir / "data", train=False, download=True, transform=MNIST_TRANSFORM)
+
+    # # Return a subset of the original datasets
+    mnist_trainset = Subset(mnist_trainset, indices=range(trainset_size))
+    mnist_testset = Subset(mnist_testset, indices=range(testset_size))
+
+    return mnist_trainset, mnist_testset
+
+
+mnist_trainset, mnist_testset = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+mnist_testloader = DataLoader(mnist_testset, batch_size=64, shuffle=False)
+
+# Get the first batch of test data, by starting to iterate over `mnist_testloader`
+for img_batch, label_batch in mnist_testloader:
+    print(f"{img_batch.shape=}\n{label_batch.shape=}\n")
+    break
+
+# Get the first datapoint in the test set, by starting to iterate over `mnist_testset`
+for img, label in mnist_testset:
+    print(f"{img.shape=}\n{label=}\n")
+    break
+
+torch.testing.assert_close(img, img_batch[0])
+assert label == label_batch[0].item()
+
+# %%
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+
+# If this is CPU, we recommend figuring out how to get cuda access (or MPS if you're on a Mac).
+print(device)
+
+model = SimpleMLP().to(device)
+
+batch_size = 128
+epochs = 3
+
+mnist_trainset, _ = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=batch_size, shuffle=True)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+loss_list = []
+
+for epoch in range(epochs):
+    pbar = tqdm(mnist_trainloader)
+
+    for imgs, labels in pbar:
+        # Move data to device, perform forward pass
+        imgs, labels = imgs.to(device), labels.to(device)
+        logits = model(imgs)
+
+        # Calculate loss, perform backward pass
+        loss = F.cross_entropy(logits, labels)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # Update logs & progress bar
+        loss_list.append(loss.item())
+        pbar.set_postfix(epoch=f"{epoch + 1}/{epochs}", loss=f"{loss:.3f}")
+
+line(
+    loss_list,
+    x_max=epochs * len(mnist_trainset),
+    labels={"x": "Examples seen", "y": "Cross entropy loss"},
+    title="SimpleMLP training on MNIST",
+    width=700,
+)
+
+# %%
+@dataclass
+class SimpleMLPTrainingArgs:
+    """
+    Defining this class implicitly creates an __init__ method, which sets arguments as below, e.g. self.batch_size=64.
+    Any of these fields can also be overridden when you create an instance, e.g. SimpleMLPTrainingArgs(batch_size=128).
+    """
+
+    batch_size: int = 64
+    epochs: int = 3
+    learning_rate: float = 1e-3
+
+
+def train(args: SimpleMLPTrainingArgs) -> tuple[list[float], SimpleMLP]:
+    """
+    Trains & returns the model, using training parameters from the `args` object. Returns the model, and loss list.
+    """
+    model = SimpleMLP().to(device)
+
+    mnist_trainset, _ = get_mnist()
+    mnist_trainloader = DataLoader(mnist_trainset, batch_size=args.batch_size, shuffle=True)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    loss_list = []
+    accuracy_list = []
+
+    for epoch in range(args.epochs):
+        pbar = tqdm(mnist_trainloader)
+
+        for imgs, labels in pbar:
+            # Move data to device, perform forward pass
+            imgs, labels = imgs.to(device), labels.to(device)
+            logits = model(imgs)
+
+            # Calculate loss, perform backward pass
+            loss = F.cross_entropy(logits, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Update logs & progress bar
+            loss_list.append(loss.item())
+            pbar.set_postfix(epoch=f"{epoch + 1}/{epochs}", loss=f"{loss:.3f}")
+
+        # Validation loop
+        with torch.inference_mode():
+            test_acc = []
+            for imgs, labels in mnist_testloader:
+                imgs, labels = imgs.to(device), labels.to(device)
+                logits = model(imgs)
+
+                # Calculate accuracy
+                classes = torch.argmax(logits, dim=1)
+                test_acc.append(torch.mean((classes == labels).float()).item())
+            
+        accuracy_list.extend(test_acc)
+
+    return loss_list, accuracy_list, model
+
+
+args = SimpleMLPTrainingArgs()
+loss_list, accuracy_list, model = train(args)
+
+line(
+    y=[loss_list, [0.1] + accuracy_list],  # we start by assuming a uniform accuracy of 10%
+    use_secondary_yaxis=True,
+    x_max=args.epochs * len(mnist_trainset),
+    labels={"x": "Num examples seen", "y1": "Cross entropy loss", "y2": "Test Accuracy"},
+    title="SimpleMLP training on MNIST",
+    width=800,
+)
+
+
+# %%
+class Conv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding: int = 0):
+        """
+        Same as torch.nn.Conv2d with bias=False.
+
+        Name your weight field `self.weight` for compatibility with the PyTorch version.
+
+        We assume kernel is square, with height = width = `kernel_size`.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        sf = 1 / (in_channels * kernel_size**2)**0.5
+        w = torch.FloatTensor(out_channels, in_channels, kernel_size, kernel_size).uniform_(-sf, sf)
+        print(w.shape)
+        self.weight = nn.Parameter(w)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Apply the functional conv2d, which you can import."""
+        return torch.nn.functional.conv2d(x, self.weight, stride=self.stride, padding=self.padding)
+
+    def extra_repr(self) -> str:
+        keys = ["in_channels", "out_channels", "kernel_size", "stride", "padding"]
+        return ", ".join([f"{key}={getattr(self, key)}" for key in keys])
+
+
+tests.test_conv2d_module(Conv2d)
+m = Conv2d(in_channels=24, out_channels=12, kernel_size=3, stride=2, padding=1)
+print(f"Manually verify that this is an informative repr: {m}")
+
+
+# %%
+class MaxPool2d(nn.Module):
+    def __init__(self, kernel_size: int, stride: int | None = None, padding: int = 1):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Call the functional version of maxpool2d."""
+        return F.max_pool2d(x, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding)
+
+    def extra_repr(self) -> str:
+        """Add additional information to the string representation of this class."""
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["kernel_size", "stride", "padding"]])
+
+
+# %%
+class Sequential(nn.Module):
+    _modules: dict[str, nn.Module]
+
+    def __init__(self, *modules: nn.Module):
+        super().__init__()
+        for index, mod in enumerate(modules):
+            self._modules[str(index)] = mod
+
+    def __getitem__(self, index: int) -> nn.Module:
+        index %= len(self._modules)  # deal with negative indices
+        return self._modules[str(index)]
+
+    def __setitem__(self, index: int, module: nn.Module) -> None:
+        index %= len(self._modules)  # deal with negative indices
+        self._modules[str(index)] = module
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Chain each module together, with the output from one feeding into the next one."""
+        for mod in self._modules.values():
+            x = mod(x)
+        return x
+
+# %%
+# BatchNorm2d
+
