662a27b0280b284e5f7a73ca9dbde169671bfc36
William Y. Feng <womogenes2@gmail.com>
2025-06-21 14:26:37 -0400

    Implement attention

--- chapter1_transformer_interp/exercises/part1_transformer_from_scratch/1.1_transformer.py
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/1.1_transformer.py
@@ -163,3 +163,147 @@ class LayerNorm(nn.Module):
 
 rand_float_test(LayerNorm, [2, 4, 768])
 load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
+
+# %% Embed
+
+class Embed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(t.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"])-> Float[Tensor, "batch position d_model"]:
+        # Indexing magic
+        return self.W_E[tokens]
+
+
+rand_int_test(Embed, [2, 4])
+load_gpt2_test(Embed, reference_gpt2.embed, tokens)
+
+# %% Positional embeddings
+
+class PosEmbed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_pos = nn.Parameter(t.empty((cfg.n_ctx, cfg.d_model)))
+        nn.init.normal_(self.W_pos, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+        _, seq_len = tokens.shape
+        return self.W_pos[None, :seq_len]
+
+
+rand_int_test(PosEmbed, [2, 4])
+load_gpt2_test(PosEmbed, reference_gpt2.pos_embed, tokens)
+
+
+# %% apply_causal_mask
+
+class Attention(nn.Module):
+    IGNORE: Float[Tensor, ""]
+
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.register_buffer("IGNORE", t.tensor(float("-inf"), dtype=t.float32, device=device))
+
+    def apply_causal_mask(
+        self,
+        attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"],
+    ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+        """
+        Applies a causal mask to attention scores, and returns masked scores.
+        """
+        batch, n_heads, query_pos, key_pos = attn_scores.shape
+        mask_2d = torch.triu(torch.ones(query_pos, key_pos, dtype=bool), diagonal=1)
+        masked_scores = torch.where(
+            mask_2d[None, None, :, :], self.IGNORE, attn_scores)
+
+        return masked_scores
+
+
+tests.test_causal_mask(Attention.apply_causal_mask)
+
+
+# %% Attention
+
+class Attention(nn.Module):
+    IGNORE: Float[Tensor, ""]
+
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_Q = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_K = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_V = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_O = nn.Parameter(t.empty((cfg.n_heads, cfg.d_head, cfg.d_model)))
+        self.b_Q = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_K = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_V = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_O = nn.Parameter(t.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_Q, std=self.cfg.init_range)
+        nn.init.normal_(self.W_K, std=self.cfg.init_range)
+        nn.init.normal_(self.W_V, std=self.cfg.init_range)
+        nn.init.normal_(self.W_O, std=self.cfg.init_range)
+        self.register_buffer(
+            "IGNORE",
+            t.tensor(float("-inf"), dtype=t.float32, device=device)
+        )
+
+    def forward(self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        batch, posn, d_model = normalized_resid_pre.shape
+        reshaped_resid_pre = normalized_resid_pre[:, :, :, None]
+
+        q = einops.einsum(
+            normalized_resid_pre, self.W_Q,
+            "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head"
+        ) + self.b_Q
+        k = einops.einsum(
+            normalized_resid_pre, self.W_K,
+            "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head"
+        ) + self.b_K
+        v = einops.einsum(
+            normalized_resid_pre, self.W_V,
+            "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head"
+        ) + self.b_V
+        
+        attn_scores = einops.einsum(
+            q, k,
+            "batch posn_q nheads d_head, batch posn_k nheads d_head -> "
+            "batch nheads posn_q posn_k"
+        ) / (self.cfg.d_head**0.5)
+        masked_attn_scores = self.apply_causal_mask(attn_scores)
+
+        # Apply softmax over key dimension
+        attn_probs = torch.softmax(masked_attn_scores, dim=3)
+        z = einops.einsum(
+            attn_probs, v,
+            "batch nheads posn_q posn_k, batch posn_k nheads d_head -> "
+            "batch posn_q nheads d_head"
+        )
+        return einops.einsum(
+            self.W_O, z,
+            "nheads d_head d_model, batch posn nheads d_head -> "
+            "batch posn d_model"
+        ) + self.b_O
+
+    def apply_causal_mask(
+        self, attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"]
+    ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+        """
+        Applies a causal mask to attention scores, and returns masked scores.
+        """
+        batch, n_heads, query_pos, key_pos = attn_scores.shape
+        mask_2d = torch.triu(torch.ones(query_pos, key_pos, dtype=bool), diagonal=1)
+        masked_scores = torch.where(
+            mask_2d[None, None, :, :], self.IGNORE, attn_scores)
+        return masked_scores
+
+
+tests.test_causal_mask(Attention.apply_causal_mask)
+rand_float_test(Attention, [2, 4, 768])
+load_gpt2_test(Attention, reference_gpt2.blocks[0].attn, cache["normalized", 0, "ln1"])
+
+
