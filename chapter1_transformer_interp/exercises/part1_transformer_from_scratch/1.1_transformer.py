0257b142fc2edde42270f632831d7ffa7f17fa09
William Y. Feng <womogenes2@gmail.com>
2025-06-21 13:22:25 -0400

    Implement LayerNorm

---
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/1.1_transformer.py
@@ -0,0 +1,165 @@
+# %%
+
+## 1.1 Transformers from scratch
+
+# %%
+
+import math
+import os
+import sys
+import webbrowser
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable
+
+import datasets
+import einops
+import numpy as np
+import torch as t
+import torch
+import torch.nn as nn
+import wandb
+from jaxtyping import Float, Int
+from rich import print as rprint
+from rich.table import Table
+from torch import Tensor
+from torch.utils.data import DataLoader
+from tqdm.notebook import tqdm
+from transformer_lens import HookedTransformer
+from transformer_lens.utils import gelu_new, tokenize_and_concatenate
+from transformers.models.gpt2.tokenization_gpt2_fast import GPT2TokenizerFast
+
+device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
+
+# Make sure exercises are in the path
+chapter = "chapter1_transformer_interp"
+section = "part1_transformer_from_scratch"
+root_dir = next(p for p in Path.cwd().parents if (p / chapter).exists())
+exercises_dir = root_dir / chapter / "exercises"
+section_dir = exercises_dir / section
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
+
+import part1_transformer_from_scratch.solutions as solutions
+import part1_transformer_from_scratch.tests as tests
+from plotly_utils import imshow
+
+MAIN = __name__ == "__main__"
+
+# %% GPT-2 demo
+
+reference_gpt2 = HookedTransformer.from_pretrained(
+    "gpt2-small",
+    fold_ln=False,
+    center_unembed=False,
+    center_writing_weights=False,  # you'll learn about these arguments later!
+)
+
+sorted_vocab = sorted(list(reference_gpt2.tokenizer.vocab.items()), key=lambda n: n[1])
+print(sorted_vocab[:20])
+print()
+print(sorted_vocab[250:270])
+print()
+print(sorted_vocab[990:1010])
+print()
+
+lengths = dict.fromkeys(range(3, 8), "")
+for tok, idx in sorted_vocab:
+    if not lengths.get(len(tok), True):
+        lengths[len(tok)] = tok
+
+for length, tok in lengths.items():
+    print(f"{length}: {tok}")
+
+
+# %% Tokens
+
+reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+tokens = reference_gpt2.to_tokens(reference_text).to(device)
+print(tokens)
+print(tokens.shape)
+print(reference_gpt2.to_str_tokens(tokens))
+
+logits, cache = reference_gpt2.run_with_cache(tokens)
+print(logits.shape)
+
+# %% Config
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+
+cfg = Config()
+print(cfg)
+
+# %% Tests
+
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randn(shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple):
+        output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randint(100, 1000, shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple):
+        output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+
+def load_gpt2_test(cls, gpt2_layer, input):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    print("Input shape:", input.shape)
+    output = layer(input)
+    if isinstance(output, tuple):
+        output = output[0]
+    print("Output shape:", output.shape)
+    try:
+        reference_output = gpt2_layer(input)
+    except:
+        reference_output = gpt2_layer(input, input, input)
+    print("Reference output shape:", reference_output.shape, "\n")
+    comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum() / comparison.numel():.2%} of the values are correct\n")
+    assert 1 - (comparison.sum() / comparison.numel()) < 1e-5, "More than 0.01% of the values are incorrect"
+
+# %% LayerNorm
+
+class LayerNorm(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(t.ones(cfg.d_model))
+        self.b = nn.Parameter(t.zeros(cfg.d_model))
+
+    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        mean = torch.mean(residual, dim=2, keepdim=True)
+        var = torch.var(residual, dim=2, keepdim=True, unbiased=False)
+        denom = torch.sqrt(var + self.cfg.layer_norm_eps)
+        
+        return (residual - mean) / denom * self.w[None, None, :] + self.b[None, None, :]
+
+
+rand_float_test(LayerNorm, [2, 4, 768])
+load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
